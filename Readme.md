a29c2155b09c7264f2179cf646f594c43655c1ea
Vijayavedhasekarn <vedhatech02@gmail.com>
2024-02-24 23:20:44 +0530

    connect wit database using mongoose

---
+++ Readme.md
@@ -0,0 +1,21 @@
+# Simple crud express + mongoDb
+
+## setup mongodb
+
+> step1: visit mongodb atlas website:[link](https://www.mongodb.com/atlas)
+
+> step2: signin and create a new project
+
+> step3: finish all configaration
+
+> step4: setup authentication via username and password
+
+> step5: configure IP for project
+
+> step6 : go to database section and add database using MongoDB Driver
+
+> step7: install mongoDB in our project using **npm install mongodb**
+
+> step8: install mongoose and import mongoose package in our code
+
+> step9: make connection with DB using mongoose with connection string copied from mongoDB/atlas website
--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
-
-app.listen(3000, () => {
-  console.log("server is running on port 3000");
-});
 
 app.get("/", (req, res) => {
   res.send("hello from node Api");
 });
+
+// mongoose
+mongoose
+  .connect(
+    "mongodb+srv://vedhatech:vREnPpwDB0BGdYWD@backenddb.xqsb0sj.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB"
+  )
+  .then(() => {
+    console.log("conected to database!");
+    app.listen(3000, () => {
+      console.log("server is running on port 3000");
+    });
+  })
+  .catch(() => {
+    console.log("conection failed!");
+  });
