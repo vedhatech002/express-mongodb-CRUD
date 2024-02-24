ab6dd5255e057aff9775683bc3a20d33fe7fa624
Vijayavedhasekarn <vedhatech02@gmail.com>
2024-02-24 22:21:27 +0530

    craeting a hello world Api using express

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("hello there");
+const express = require("express");
+const app = express();
+
+app.listen(3000, () => {
+  console.log("server is running on port 3000");
+});
+
+app.get("/", (req, res) => {
+  res.send("hello from node Api");
+});
